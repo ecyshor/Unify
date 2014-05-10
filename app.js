8f1dbe6c61557618688f5fb916b89071fe83e9df
Reut Nicu <reutnicu@gmail.com>
2014-05-11 01:24:56 +0300

    Added chat integration for multiple users.

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
  */
 
 var express = require('express');
+var sio = require('socket.io');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -28,9 +28,40 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+var server = http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
+/**
+ * Socket.IO server (single process only)
+ */
+
+var io = sio.listen(server)
+    , nicknames = {};
+
+io.sockets.on('connection', function (socket) {
+    socket.on('user message', function (msg) {
+        socket.broadcast.emit('user message', socket.nickname, msg);
+    });
+
+    socket.on('nickname', function (nick, fn) {
+        if (nicknames[nick]) {
+            fn(true);
+        } else {
+            fn(false);
+            nicknames[nick] = socket.nickname = nick;
+            socket.broadcast.emit('announcement', nick + ' connected');
+            io.sockets.emit('nicknames', nicknames);
+        }
+    });
+
+    socket.on('disconnect', function () {
+        if (!socket.nickname) return;
+
+        delete nicknames[socket.nickname];
+        socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
+        socket.broadcast.emit('nicknames', nicknames);
+    });
+});
+
+app.get('/', routes.index);
