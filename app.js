053021b7d752f51d8b3e9c9fcc707e858a0f5349
Reut Nicu <reutnicu@gmail.com>
2014-05-11 23:04:53 +0300

    Added authentication through auth0, mongoDb connection.

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express');
-var sio = require('socket.io');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
-
 var app = express();
-
+var webSocket = require('./lib/setup_websocket.js');
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -22,10 +21,14 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.cookieParser());
+app.use(express.session({secret: 'sesss'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 
@@ -35,33 +38,14 @@ var server = http.createServer(app).listen(app.get('port'), function () {
 /**
  * Socket.IO server (single process only)
  */
+webSocket.configWebSocket(server);
 
-var io = sio.listen(server)
-    , nicknames = {};
-
-io.sockets.on('connection', function (socket) {
-    socket.on('user message', function (msg) {
-        socket.broadcast.emit('user message', socket.nickname, msg);
-    });
-
-    socket.on('nickname', function (nick, fn) {
-        if (nicknames[nick]) {
-            fn(true);
-        } else {
-            fn(false);
-            nicknames[nick] = socket.nickname = nick;
-            socket.broadcast.emit('announcement', nick + ' connected');
-            io.sockets.emit('nicknames', nicknames);
-        }
-    });
-
-    socket.on('disconnect', function () {
-        if (!socket.nickname) return;
-
-        delete nicknames[socket.nickname];
-        socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
-        socket.broadcast.emit('nicknames', nicknames);
-    });
-});
 
 app.get('/', routes.index);
+app.get('/callback',
+    passport.authenticate('auth0'),
+    function (req, res) {
+        res.redirect("/");
+    });
+
+
